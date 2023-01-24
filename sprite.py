461617b6fb6a03bb4d9c194a3211083180121dc0
Gaurav Agarwal <gauravagarwal001@outlook.com>
2023-01-24 17:39:02 +0530

    Modified Sprite Class

--- sprite.py
+++ sprite.py
@@ -3,54 +3,23 @@ from math import sqrt
 import numpy as np
 
 class Sprite :
-    def __init__ (self, impath, size, max_speed=(2,0), acceleration=0.1):
+    def __init__ (self, impath, scale : int = 1, direction : int = 1, max_angular_vel : float = 5):
+
+        # Direction can only be +1 or -1 -> For Direction of Angular Velocity.
+
         self.image = pygame.image.load(impath)
         self.rect = self.image.get_rect()
-        self.max_speed = max_speed
-        self.speed = (0,0)
-        self.acceleration=acceleration
-        self.position = 0.0
-
-        self.target = None
-        self.old_target = (0,0)
-
-        self.screen = size
-        self.position = 50
-        self.old_position=49
+        self.scale = scale
+        self.direction = direction
+        self.max_angular_vel = max_angular_vel   
+        
 
     def blit(self,screen):
         screen.blit(self.image, self.rect)
 
-    def random_walk (self):
-        if self.target == None:
-            choice = np.random.randint(2)
-            self.position += 1 if choice else -1
-            self.target = (self.position*15,self.rect.centery)
-            print("targetting",self.target)
-        else:
-            if abs(self.rect.centerx - self.target[0]) < 8:
-                self.old_position = self.position
-                self.target = None
-                self.random_walk()
-                return
-            self.move_towards(self.target)
-        
-    def move_towards (self, point):
-        #self.origin()
-        factor = (self.target[0] - self.rect.centerx,
-                  self.target[1] - self.rect.centery)
-        factor = normalise(factor)
-        """self.speed = tuple(map(
-                    op("+"),
-                    tuple(map(
-                        op("*"),self.acceleration,factor
-                        )),
-                    self.speed
-                    ))"""
-        self.speed = tuple(map(op("*"),self.max_speed,factor))
-        #self.position = tuple(map(op("+"),self.position, self.speed))
-        #self.rect.move(self.position)
-        self.rect.move_ip(self.speed[0],self.speed[1])
+    def rotate(self, degrees : int = 1) : 
+        self.image = pygame.transform.rotate(self.image, degrees*self.direction)
+        self.rect = self.image.get_rect()
     
     def origin (self):
         self.rect.left=self.position*15
