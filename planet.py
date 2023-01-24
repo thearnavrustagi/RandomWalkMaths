d7cd6a6173416e520af3e1ca9662eb4df34dc213
Arnav Rustagi <arnrustagi@gmail.com>
2023-01-24 17:40:46 +0530

    added planet

---
+++ planet.py
@@ -0,0 +1,15 @@
+from sprite import Sprite
+import pygame
+
+DEGREES = 360
+
+class Planet (Sprite):
+    def __init__ (self):
+        self.steps = 0
+
+    def random_walk (divisions=90):
+        if self.steps > divisions/DEGREES:
+            self.steps = 0
+            self.direction = -1 if np.random.randint(2) else 1
+        self.rotate(1)
+        self.steps += 1
